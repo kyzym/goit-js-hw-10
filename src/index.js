45c85c57c5a65fdeffc8cc9077acb9d5cf52d4d0
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-09-17 19:58:46 +0300

    last?

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix';
-import { fetchCountries } from './js/fetch-countries';
+import { fetchCountries } from './fetch-countries';
 
 const DEBOUNCE_DELAY = 300;
 
